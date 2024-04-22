a94d86deef7e81deb1bb4012ada90cc59654a8d5
emilqw <emilyayv@gmail.com>
2024-04-22 21:19:49 +0300

    Server Init

---
+++ Server/src/test/java/com/nebrora/sa/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nebrora.sa.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
